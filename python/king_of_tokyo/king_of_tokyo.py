8e77ea15f0a444d8fa7ac9283e8f78cca885a9dd
John Broxton <johnbroxton@gmail.com>
2015-08-25 11:10:03 -0700

    Update King of Tokyo

--- python/king_of_tokyo/king_of_tokyo.py
+++ python/king_of_tokyo/king_of_tokyo.py
@@ -3,6 +3,9 @@ __author__ = 'jbroxton'
 class Monster:
 
     def __init__(self):
+        """The Monster Class controls the parameters for a giant, city-smashing
+        monster. The Monster initializes with a name, a status of 'Out of
+        Tokyo', health = 10, and 0 victory points."""
 
         self.name = "Bojangles the Howling Leviathan"
         self.status = "Out of Tokyo"
@@ -10,18 +13,27 @@ class Monster:
         self.victory_points = 0
 
     def reset(self):
+        """The reset function sets the Monster's status, health and victory
+        points to their original values"""
+
         self.status = "Out of Tokyo"
         self.health = 10
         self.victory_points = 0
 
     def in_tokyo(self):
+        """The in_tokyo function returns a bool, True for 'In Tokyo', False
+        for 'Out of Tokyo' """
+
         if self.status == "In Tokyo":
             return True
-        elif self.status == "Out of Tokyo":
+        else:
             return False
 
     def flee(self):
-        answer = input("Do you want to flee Tokyo? >>> ")
+        """Return True if the monster would like to flee Tokyo, False for
+        anything else. """
+
+        answer = input("Do you want to flee Tokyo? (y or n) >>> ")
         answer = answer.lower()
         if answer == "y":
             return True
@@ -29,21 +41,32 @@ class Monster:
             return False
 
     def heal(self, balm):
-        if self.health + balm <= 10:
+        """The heal function passes in a balm integer and adds it to the
+        Monster's health. The health cannot exceed 10 """
+
+        balm = int(balm)
+        if self.health + balm < 10:
             self.health = self.health + balm
         elif self.health + balm >= 10:
             self.health = 10
 
     def attack(self, damage):
+        """The attack function takes a damage integer and subtracts it from
+         the Monster's health. If the health is less than or equal to zero,
+         the health is 'K.O.'d' """
+
+        damage = int(damage)
         if self.health - damage > 0:
             self.health = self.health - damage
         elif self.health - damage <= 0:
             self.health = "K.O.'d"
+        return self.health
 
     def score(self, wins):
         if self.victory_points + wins < 20:
             self.victory_points = self.victory_points + wins
         elif self.victory_points + wins >= 20:
             self.victory_points = "WINNING"
+        return self.victory_points
 
 
--- python/king_of_tokyo/test_king_of_tokyo.py
+++ python/king_of_tokyo/test_king_of_tokyo.py
@@ -6,7 +6,7 @@ from unittest.mock import patch
 
 
 class TestMonster(unittest.TestCase):
-    """A 'tonster' is a test monster."""
+    """Unittests for the Monster Class. A 'tonster' is a test monster."""
 
     def setUp(self):
         self.tonster = Monster()
@@ -25,42 +25,67 @@ class TestMonster(unittest.TestCase):
         self.assertEqual(self.tonster.health, 10)
         self.assertEqual(self.tonster.victory_points, 0)
 
-    def test_in_tokyo_function(self):
-        """Test to determine where the monster is"""
+    def test_in_tokyo_yes_function(self):
+        """Test to determine if the in_tokyo function returns the correct
+         value when the Monster is In Tokyo """
         self.tonster.status = "In Tokyo"
-
         self.tonster.in_tokyo()
+        self.assertTrue(self.tonster.status)
 
+    def test_in_tokyo_no_function(self):
+        """Test to determine if in_tokyo return the correct value when the
+        Monster is Out of Tokyo"""
+        self.tonster.status = "Out of Tokyo"
+        self.tonster.in_tokyo()
         self.assertTrue(self.tonster.status)
 
     @patch('builtins.input', return_value= "n")
     def test_flee_function(self, input_value):
         """This test takes a no value and tests to see if the monster flees Tokyo"""
         response = self.tonster.flee()
-
         self.assertFalse(response)
 
-    def test_healing_function(self):
-        """Set the health to one, pass in 4 healing points and compare the returned value to 5"""
-        self.tonster.health = 1
+    @patch('builtins.input', return_value= "y")
+    def test_flee_function(self, input_value):
+        """This test takes a yes value and tests to see if the
+        monster flees Tokyo"""
+        response = self.tonster.flee()
+        self.assertTrue(response)
 
+    def test_healing_function(self):
+        """Set the health to one, pass in 4 healing points and compare the
+        returned value to 5"""
+
+        self.tonster.health = 1
         self.tonster.heal(4)
 
         self.assertEqual(self.tonster.health, 5)
 
-    def test_attack_function(self):
-        """Set the monster's health to 5, drop it by 5 points and see if the monster is K.O.'d """
-        self.tonster.health = 5
+    def test_healing_function_greater_than_ten(self):
+        """Set the health to nine, pass in 4 healing points and compare the
+        returned value to 10 (the Monster cannot have a health greater
+        than 10)"""
 
+        self.tonster.health = 9
+        self.tonster.heal(4)
+
+        self.assertEqual(self.tonster.health, 10)
+
+    def test_attack_function(self):
+        """Set the monster's health to 5, drop it by 5 points and see if the
+         monster is K.O.'d """
+
+        self.tonster.health = 5
         self.tonster.attack(5)
 
         self.assertEqual(self.tonster.health, "K.O.'d")
 
 
     def test_score_function(self):
-        """Set the monster's victory points to 15, increase by 6 and expect 'Winning' returned value """
-        self.tonster.victory_points = 15
+        """Set the monster's victory points to 15, increase by 6 and expect
+         'Winning' returned value """
 
+        self.tonster.victory_points = 15
         self.tonster.score(6)
 
         self.assertEqual(self.tonster.victory_points, "WINNING")
